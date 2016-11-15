e9cf6bef13eac5c2ff106fdd393a2439135651b0
David-McNiven <davidmcniven@bell.net>
2016-11-15 16:50:16 -0500

    route in app.js

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var db = require('./config/globalVars');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var db = require('./config/globalVars');
+var restaurants = require('./routes/restaurants')
 
 var app = express();
 
@@ -25,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/restaurants', restaurants);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
