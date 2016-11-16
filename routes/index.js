9ee8198ee9ab664aabfaf74773b5a83a9c97fffb
David-McNiven <davidmcniven@bell.net>
2016-11-16 16:06:14 -0500

    start of routing

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+
 module.exports = router;
