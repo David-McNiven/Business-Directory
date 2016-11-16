dd83b495402f08709550894c2a25463ab6a215df
David-McNiven <davidmcniven@bell.net>
2016-11-16 12:47:18 -0500

    unbroke views

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var Restaurant = require('../models/restaurant');
 
 router.get('/', function(req, res, next) {
-  Movie.find(function(err, restaurants){
+  Restaurant.find(function(err, restaurants){
     if (err) {
       res.redirect('error');
     } else {
-      res.render('restaurants', { title: 'Restaurants', restaurant: restaurants });
+      res.render('restaurants', { title: 'Restaurants', restaurants: restaurants });
     }
   });
 });
