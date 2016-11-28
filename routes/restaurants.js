5daeb1b1c63b529f0bbd69f4cd7e6a8e8865aa2b
David-McNiven <davidmcniven@bell.net>
2016-11-28 18:51:37 -0500

    delete confirmation

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -67,13 +67,17 @@ router.post('/:id', isAuth, function(req, res, next) {
 
 // attempt to delete selected restaurant from database on POST request
 router.get('/delete/:id', isAuth, function(req, res, next) {
-  Restaurant.remove({ _id: req.params.id }, function(err) {
-    if (err) {
-      res.redirect('/error');
-    } else {
-      res.redirect('/restaurants');
-    }
-  });
+  if (confirm('Are you sure you?')) {
+    Restaurant.remove({ _id: req.params.id }, function(err) {
+      if (err) {
+        res.redirect('/error');
+      } else {
+        res.redirect('/restaurants');
+      }
+    });
+  } else {
+    res.redirect('/restaurants');
+  }
 });
 
 // user authentication check for all CRUD operations
